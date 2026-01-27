9bf7cc061659e5f1d5fce2663dd87d41f943a15f
vishakhshetty11 <vishakhshetty11@gmail.com>
2026-01-27 21:59:00 +0530

    time out issue  resolved

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,23 +1,31 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import FlagCard from "../components/FlagCard";
 const HomePage = () => {
     const [data, setData] = useState([])
-    function getFlagData(){
-        fetch('https://xcountries-backend.labs.crio.do/all')
-            .then(res => res.json())
-            .then(resdata => setData(resdata))
-            .catch(err => console.error(err));
-    }
-    useEffect(() =>{
+    const getFlagData = async () => {
+        try {
+            const res = await fetch("https://xcountries-backend.labs.crio.do/all");
+
+            if (!res.ok) {
+                throw new Error("Network response was not ok");
+            }
+
+            const resdata = await res.json();
+            setData(resdata);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+    useEffect(() => {
         getFlagData();
-    },[])
+    }, [])
     return (
-        <div style={{display: "flex", flexDirection:"row", flexWrap:"wrap", gap:"10px", justifyContent:"center"}}>
-            {data.length >0 &&
-            data.map(item =>(
-                <FlagCard countryName={item.name} img={item.flag} alt={item.abbr} />
-            ))}
-            
+        <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", gap: "10px", justifyContent: "center" }}>
+            {data.length > 0 &&
+                data.map(item => (
+                    <FlagCard countryName={item.name} img={item.flag} alt={item.abbr} />
+                ))}
+
         </div>)
 }
 export default HomePage;
