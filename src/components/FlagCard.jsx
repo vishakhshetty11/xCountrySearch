2cea2e980aa079521e20c5007cfad60bd43597e2
vishakhshetty11 <vishakhshetty11@gmail.com>
2026-02-14 01:23:42 +0530

    Added Class Name countryCard

--- src/components/FlagCard.jsx
+++ src/components/FlagCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FlagCard = ({countryName, img, alt}) => {
     return (
-        <div style={{width:"150px", height:"150px", border: "1px solid gray", padding:"10px"}}>
+        <div className='countryCard' style={{width:"150px", height:"150px", border: "1px solid gray", padding:"10px"}}>
             <img src={img} alt={alt} 
             style={{width:"100px"}} />
             <h4>{countryName}</h4>
